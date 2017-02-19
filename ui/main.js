49af5317a6a901cb8b33ed5fcb18a39f825f0503
Kruthika4m <kruthi.4m@gmail.com>
2017-02-19 22:47:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,9 @@ var element = document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
   marginLeft=marginLeft+10;
-  element.style.marginleft=marginLeft+'px';
+  element.style.marginleft = marginLeft+'px';
     
 }
 element.onclick= function(){
-     var interval= setInterval(moveRight , 100);
+     var interval= setInterval( moveRight , 100);
 };
